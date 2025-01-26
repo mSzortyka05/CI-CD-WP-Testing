efff1ac2b6c3d2e5ba8e747bf9b4dba89bb363ba
SzortykaZSK <mateusz.szortyka@uczen.zsk.poznan.pl>
2025-01-26 17:09:57 +0100

    Add some functionality to functions.php and add test for them

--- tests/Test.php
+++ tests/Test.php
@@ -1,8 +1,46 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require_once dirname(__DIR__) . '/wordpress/wp-content/themes/twentytwentyfive/functions.php';
+
 class Test extends TestCase {
+    //test domyslnie przechodzacy
     public function testExample() {
         $this->assertTrue(true);
+    }
+    // Test funkcji mytheme_generate_excerpt
+    public function testGenerateExcerpt() {
+        $text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+        
+        // Skrót krótszy niż cały tekst
+        $excerpt = mytheme_generate_excerpt($text, 20);
+        $this->assertEquals("Lorem ipsum dolor s...", $excerpt);
+
+        // Skrót równy tekstowi
+        $excerpt = mytheme_generate_excerpt($text, 100);
+        $this->assertEquals($text, $excerpt);
+
+        // Skrót pustego tekstu
+        $excerpt = mytheme_generate_excerpt("", 10);
+        $this->assertEquals("", $excerpt);
+    }
+
+    // Test funkcji mytheme_format_price
+    public function testFormatPrice() {
+        // Poprawne formatowanie
+        $formattedPrice = mytheme_format_price(1234.56);
+        $this->assertEquals("1 234,56 zł", $formattedPrice);
+
+        // Formatowanie liczby całkowitej
+        $formattedPrice = mytheme_format_price(1000);
+        $this->assertEquals("1 000,00 zł", $formattedPrice);
+
+        // Formatowanie wartości zero
+        $formattedPrice = mytheme_format_price(0);
+        $this->assertEquals("0,00 zł", $formattedPrice);
+
+        // Niepoprawny typ danych
+        $formattedPrice = mytheme_format_price("abc");
+        $this->assertFalse($formattedPrice);
     }
 }
--- wordpress/wp-content/themes/twentytwentyfive/functions.php
+++ wordpress/wp-content/themes/twentytwentyfive/functions.php
@@ -156,3 +156,19 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
+
+// Generowanie skrótu tekstu o określonej długości
+function mytheme_generate_excerpt($text, $length = 100) {
+    if (strlen($text) <= $length) {
+        return $text;
+    }
+    return substr($text, 0, $length) . '...';
+}
+
+// Formatowanie ceny w walucie PLN
+function mytheme_format_price($price) {
+    if (!is_numeric($price)) {
+        return false;
+    }
+    return number_format($price, 2, ',', ' ') . ' zł';
+}
